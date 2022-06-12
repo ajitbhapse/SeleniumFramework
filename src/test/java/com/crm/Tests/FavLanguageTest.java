c77c518af65de12ce993687539c58fda1e9a07eb
ajitbhapse <ajitbhapse@gmail.com>
2022-06-12 21:05:27 +0530

    Update FavLanguageTest.java

--- src/test/java/com/crm/Tests/FavLanguageTest.java
+++ src/test/java/com/crm/Tests/FavLanguageTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import org.testng.Assert;
 import com.crm.pages.InputtyperadioPage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
